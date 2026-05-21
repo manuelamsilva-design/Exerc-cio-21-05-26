6f173f39a751c1c2eb6bff59b233b3a5226e087d
manuelamsilva-design <manuela_m_silva@estudante.sesisenai.org.br>
2026-05-21 12:34:02 -0300

    exercício2

---
+++ exercicio2.js
@@ -0,0 +1,9 @@
+//Manuela Urbano 2A
+
+const numero = 1442
+
+if (numero % 2 ===  0) {
+    console.log("O numero " + numero + " é par")
+} else {
+    console.log("O numero " + numero + " é impar")
+}
